c7e9cbe653e0668f814c0515ae0a24ae3a5e6d96
Jose Samuel <lopeztorresjosesamuel@gmail.com>
2026-05-07 00:37:19 -0500

    fix: disable ONLY_FULL_GROUP_BY in PDO connection for MySQL 8 compatibility

--- config/database.php
+++ config/database.php
@@ -37,6 +37,7 @@ function getDBConnection(): PDO
                 PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                 PDO::ATTR_EMULATE_PREPARES   => false,
+                PDO::MYSQL_ATTR_INIT_COMMAND => "SET SESSION sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''))"
             ]
         );
     }
